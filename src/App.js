9ff20f8a4db57aa791bda779bd6173f74b060629
JGrada <j.grada14@gmail.com>
2022-06-04 23:25:21 +0100

    23:25 04 06

--- src/App.js
+++ src/App.js
@@ -16,26 +16,39 @@ function App() {
   const [gameStarted, setGameStarted] = useState(false);
   const [selectedLevel, setSelectedLevel] = useState("0");
   let tabuleiro = [[]];
+  let tabuleiroaux = [[]];
   let nrLinhas;
   let nrColunas;
-  let randomWords = ["Aprovado","Facil","Script","Website","React","Professor","Pedro","Rodrigo","Telmo","Grada"]
+  let nrMaximoPalavras;
+  let randomWords = ["APROVADO","FACIL","SCRIPT","WEBSITE","REACT","PROFESSOR","PEDRO","RODRIGO","TELMO","GRADA"]
+ 
 
   if(selectedLevel === "1"){
     nrLinhas = 15;
     nrColunas = 15;
+    nrMaximoPalavras = 10;
   }
   else if(selectedLevel === "2"){
     nrLinhas = 25;
     nrColunas = 25;
+    nrMaximoPalavras = 15;
+
   } 
   else if(selectedLevel === "3"){
     nrLinhas = 40;
     nrColunas = 40;
+    nrMaximoPalavras = 20;
+
   } 
+
+
+  function inverteString(s){
+    return s.split("").reverse().join("");
+}
 
   function encontraLetraRandom(){
     var resultado = '';
-    var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var caracteres = '123456789';
     var caracteresLength = caracteres.length;
    
     resultado += caracteres.charAt(Math.floor(Math.random() * caracteresLength));
@@ -53,20 +66,245 @@ function App() {
   function encontraRandomWord(){
     let posicao = encontraNrRandom();
     return randomWords[posicao];
+  }
+
+  function encontraTamanhoDaPalavra(){
+    let tamanhoPalavra = encontraRandomWord().length;
+    return tamanhoPalavra;
+  }
+    
+  function encontraRandomPosicaoTab(){
+      let posicao = Math.floor((Math.random() * 10));
+      return posicao;
 
   }
+
+  function organizaTabuleiro(){
+    let nrpalavras = 0;
+
+    do{
+
+      let nrRandom = Math.floor((Math.random() * 5));
+      console.log(nrRandom);
+        if(nrRandom === 0){
+          [tabuleiro, tabuleiroaux] = trocaLetraHorizontal();
+        }
+        else if(nrRandom == 1){
+          [tabuleiro, tabuleiroaux] = trocaLetraHorizontalReverse();
+        }
+        else if(nrRandom == 2){
+          [tabuleiro, tabuleiroaux] = trocaLetraVertical();
+        }
+        else if(nrRandom == 3){
+          [tabuleiro, tabuleiroaux] = trocaLetraVerticalReverse();
+        }
+        else if(nrRandom == 4){
+          [tabuleiro, tabuleiroaux] =  trocaLetraDiagonal();
+        }
+        else if(nrRandom == 5){
+          [tabuleiro, tabuleiroaux] =  trocaLetraDiagonalReverse();
+        }
+
+        nrpalavras++;
+      
+    }while(nrpalavras < nrMaximoPalavras)
+  }
+  
+  function preencheTabuleiroAuxiliar(){
+    tabuleiroaux = Array(nrLinhas).fill(0).map( 
+      () => new Array(nrColunas).fill(0).map( 
+          () => " "
+      ) )
+
+      /*
+      console.log(tabuleiroaux); 
+      organizaTabuleiro();
+      console.log(tabuleiroaux); */
+      
+      }
 
   function preencheTabuleiro(){
     tabuleiro = Array(nrLinhas).fill(0).map( 
       () => new Array(nrColunas).fill(0).map( 
           () => encontraLetraRandom()
-      )
-)       
-        console.log(tabuleiro);
-        console.log(encontraRandomWord());
-          }
+      ))     
+       
+    console.log(tabuleiro);
+    organizaTabuleiro();
+    //console.log(tabuleiroaux);
+    console.log(tabuleiro);
   
-  /*When the game starts*/
+  }
+  
+  function trocaLetraHorizontal(){
+    
+        let palavra = encontraRandomWord();
+        let posicaoC;
+        let posicaoL = encontraRandomPosicaoTab();
+        let sobreposicao = false;
+        do{
+          sobreposicao = false;
+          posicaoC = encontraRandomPosicaoTab();
+          for(let p = 0; p < palavra.length; p++){
+            if(tabuleiroaux[posicaoL][posicaoC + p] !== " "){
+             sobreposicao = true;
+            } 
+          }
+          
+        }while(posicaoC + palavra.length > nrColunas || sobreposicao)
+        
+        for(let p = 0; p < palavra.length; p++){
+          if(tabuleiroaux[posicaoL][posicaoC + p] === " "){
+            tabuleiroaux[posicaoL][posicaoC + p] = palavra[p];  
+            tabuleiro[posicaoL][posicaoC + p] = palavra[p];  
+          } 
+        }
+      
+        return [tabuleiro, tabuleiroaux];
+    
+}
+  
+  function trocaLetraHorizontalReverse(){
+    
+    let palavra = encontraRandomWord();
+    palavra = inverteString(palavra);
+    let posicaoC;
+    let posicaoL = encontraRandomPosicaoTab();
+    let sobreposicao = true;
+    do{
+      sobreposicao = false;
+      posicaoC = encontraRandomPosicaoTab();
+      for(let p = 0; p < palavra.length; p++){
+        if(tabuleiroaux[posicaoL][posicaoC + p] !== " "){
+          sobreposicao = true;
+        } 
+      }
+    }while(posicaoC + palavra.length > nrColunas || sobreposicao)
+
+    for(let p = 0; p < palavra.length; p++){
+      if(tabuleiroaux[posicaoL][posicaoC + p] === " "){
+        tabuleiroaux[posicaoL][posicaoC + p] = palavra[p];  
+        tabuleiro[posicaoL][posicaoC + p] = palavra[p];  
+      } 
+   }
+   return [tabuleiro, tabuleiroaux];
+  }
+
+  function trocaLetraVertical(){
+    let palavra = encontraRandomWord();
+    let posicaoL;
+    let posicaoC = encontraRandomPosicaoTab();
+    let sobreposicao = true;
+    do{
+      sobreposicao = false;
+      posicaoL = encontraRandomPosicaoTab();
+      for(let p = 0; p < palavra.length; p++){
+        if(tabuleiroaux[posicaoL + p][posicaoC] !== " "){
+          sobreposicao = true;
+        } 
+      }
+    }while(posicaoL + palavra.length > nrLinhas || sobreposicao)
+  
+    for(let p = 0; p < palavra.length; p++){
+      if(tabuleiroaux[posicaoL + p][posicaoC ] === " "){
+        tabuleiroaux[posicaoL + p][posicaoC] = palavra[p];  
+        tabuleiro[posicaoL + p][posicaoC] = palavra[p];  
+      } 
+    }
+
+    return [tabuleiro, tabuleiroaux];
+
+  }
+  function trocaLetraVerticalReverse(){
+    
+    let palavra = encontraRandomWord();
+    palavra = inverteString(palavra);
+    let posicaoC = encontraRandomPosicaoTab();
+    let posicaoL;
+    let sobreposicao;
+    do{
+      sobreposicao = false;
+      posicaoL = encontraRandomPosicaoTab();
+      for(let p = 0; p < palavra.length; p++){
+        if(tabuleiroaux[posicaoL + p][posicaoC] !== " "){
+          sobreposicao = true;
+        } 
+      }
+    }while(posicaoL + palavra.length > nrLinhas || sobreposicao)
+  
+    for(let p = 0; p < palavra.length; p++){
+      if(tabuleiroaux[posicaoL + p][posicaoC ] === " "){
+        tabuleiroaux[posicaoL + p][posicaoC] = palavra[p];  
+        tabuleiro[posicaoL + p][posicaoC] = palavra[p];  
+      } 
+   }
+
+   return [tabuleiro, tabuleiroaux];
+
+  }
+
+  function trocaLetraDiagonal(){
+    let palavra = encontraRandomWord();
+    let posicaoL;
+    let posicaoC;
+    let sobreposicao = true;
+    do{
+      sobreposicao = false;  
+      posicaoL = encontraRandomPosicaoTab();
+      posicaoC = encontraRandomPosicaoTab();
+      for(let p = 0; p < palavra.length; p++ ){
+        if(tabuleiroaux[posicaoL + p][posicaoC + p] !== " "){
+          sobreposicao = true;
+        } 
+      }
+    
+    }while(posicaoL + palavra.length > nrLinhas || posicaoC + palavra.length > nrColunas  || sobreposicao)
+        
+
+    for(let p = 0; p < palavra.length; p++){
+      if(tabuleiroaux[posicaoL + p][posicaoC + p] === " "){
+        tabuleiroaux[posicaoL + p][posicaoC + p] = palavra[p];  
+        tabuleiro[posicaoL + p][posicaoC + p] = palavra[p];  
+      } 
+    }
+
+    return [tabuleiro, tabuleiroaux];
+  }
+
+  function trocaLetraDiagonalReverse(){
+    
+    let palavra = encontraRandomWord();
+    palavra = inverteString(palavra);
+    let posicaoL;
+    let posicaoC;
+    let sobreposicao = true;
+    do{
+      sobreposicao = false;
+      posicaoL = encontraRandomPosicaoTab();
+      for(let p = 0; p < palavra.length; p++){
+        if(tabuleiroaux[posicaoL][posicaoC + p] !== " "){
+          sobreposicao = true;
+        } 
+      }
+    
+    }while(posicaoL + palavra.length > nrLinhas || posicaoC + palavra.length > nrColunas || sobreposicao)
+
+    for(let p = 0; p < palavra.length; p++){
+      if(tabuleiroaux[posicaoL + p][posicaoC + p] === " "){
+        tabuleiroaux[posicaoL + p][posicaoC + p] = palavra[p];  
+        tabuleiro[posicaoL + p][posicaoC + p] = palavra[p];  
+      } 
+    }
+      
+    return [tabuleiro, tabuleiroaux];
+  }
+
+  
+ 
+
+  
+
+
   
 
   const handleGameStart = () => {
@@ -77,6 +315,7 @@ function App() {
     else {
       console.log("Inicia Jogo");
       setGameStarted(true);
+      preencheTabuleiroAuxiliar();
       preencheTabuleiro();
     }
   }
@@ -108,4 +347,6 @@ function App() {
   );
 }
 
+
 export default App;
+
